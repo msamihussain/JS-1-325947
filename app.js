95d94d83d3eb4b8c2fab78c31011a5641dab4cef
msamihussain <msamihussain1@gmail.com>
2024-12-18 02:26:29 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,116 @@
+//Chapter-1 Alert.....................................................................................................
+
+//question-1
+// Greet your website visitor using JS alert box.
+//alert("Welcome to my website! We're glad to have you here.");
+
+//question- 2
+// Display a message on the webpage
+//document.write("Error! Please enter a valid password.");
+
+//question-3
+// Display messages in sequence using alert boxes
+//alert("Welcome to JS land... \n Happy Coding!");
+
+//question-4
+// Display messages in sequence using alert boxes
+//alert("Welcome to JS land...")
+//alert("Happy Coding!")
+
+//question-5
+// Log the message in the console
+//console.log("Hello... I can run JS through my web browser's console")
+
+//question-6
+//use of alerts in your new/existing HTML & CSS projects
+//  document.getElementById("alertButton").addEventListener("click", function() {alert("Hello! This is your alert message.");});
+
+
+// chapter-2 Variables for Strings.................................................................................
+
+// question-1
+// var myName= ("M SAMI HUSSAIN");
+
+// question-2
+//var myName= ("M SAMI HUSSAIN");
+//alert(myName);
+
+// question-3
+// a) Declare a JS variable titled message
+ //var message;
+
+ // b) Assign "Hello World" to variable message
+// message = "Hello World";
+
+ // c) Display the message in an alert box
+ //alert(message);
+
+//question-4
+//  variables for student bio data
+// var studentName = "M SAMI HUSSAIN";
+// var studentAge = 18;
+// var studentCourse = "WEB & APP DEVELOPMENT";
+// var studentInstitute = "SMIT";
+
+// // Display the bio data in alert boxes
+// alert("Student Name: " + studentName);
+// alert("Age: " + studentAge);
+// alert("Course: " + studentCourse);
+// alert("Institute: " + studentInstitute);
+
+//question-5
+// Declare a variable containing the sequence
+// let message = "pizza\npizz\npiz\npi\np";
+
+// // Display the message in one alert box
+// alert(message);
+
+//question-6
+ // Declare a variable for email
+ //var email=("My email address is  msamihussain1@gmail.com");
+ //alert(email);
+
+ //question-7
+ //var book=("A smarter way to learn Java Script");
+//alert(book);
+
+//question-8
+//document.write("<h5>Thank you for choosing us as your next career move.We're very proud to be welcoming you to our company.</h5>")
+
+//question-9
+// Store the string in a variable
+//var specialString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+// Show the string in an alert box
+//alert(specialString);
+
+// Show the string in the browser
+//document.body.innerHTML = specialString;
+
+// chapter-3 Variables for Numbers.................................................................................
+
+//Question No 1
+
+//var age=(18);
+//alert(age);
+
+//Question-2
+//var visitors=("You have visited this site 100 times.");
+//alert(visitors);
+
+//Question No 3
+
+//var birthYear="2002"
+//document.write("<h4>" + birthYear + "</h4>");
+
+//Question No 4
+
+// var customerName = (" <b>Mr Sami</b>  ordered\n");
+// var quantity = (5);
+// var product = (" \n <b>Watches </b> on \n");
+// var store = ("AnnAmir Brand")
+
+// document.write(customerName + quantity + product + store)
+
+
+            
